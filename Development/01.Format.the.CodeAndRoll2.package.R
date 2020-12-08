ee8b6e03da428eccdd5ab6271845e8340240734b
Abel Vertesy <vertesy@users.noreply.github.com>
2020-12-08 12:38:57 +0100

    init

---
+++ Development/01.Format.the.CodeAndRoll2.package.R
@@ -0,0 +1,20 @@
+######################################################################################################
+# 01.Format.the.CodeAndRoll2.package.R
+######################################################################################################
+# source("~/MarkdownReports/Workflow_to_Create_an_R_Package.R")
+rm(list = ls(all.names = TRUE));
+try(dev.off(), silent = TRUE)
+# install.packages("devtools")
+
+# Functions ------------------------
+if (F) {
+  devtools::install_github(repo = "vertesy/RoxygenReady/RoxygenReady")
+  require("RoxygenReady")
+}
+
+RoxygenReady("~/GitHub/Packages/CodeAndRoll2/CodeAndRoll2.R")
+
+
+
+
+# Setup ------------------------
