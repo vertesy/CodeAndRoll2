f70a1cc1276e7a4884751ec0f45d4b17eb770e3b
vertesy <abel.vertesy@imba.oeaw.ac.at>
2021-10-31 21:38:09 +0100

    Reorganize - Move String parsing functionalites StringParser

--- CodeAndRoll2.R
+++ CodeAndRoll2.R
@@ -67,21 +67,6 @@ l = length
 stry <- function(...) {try(..., silent = T)} # Silent try
 
 
-ppp <- function(...) { paste(..., sep = '.') } # Paste by point
-pps <- function(...) { paste(..., sep = '/') } # Paste by (forward) slash
-ppu <- function(...) { paste(..., sep = '_') } # Paste by underscore
-ppd <- function(...) { paste(..., sep = '-') } # Paste by dash
-
-kpp <- function(...) { paste(..., sep = '.', collapse = '.') } # kollapse by point
-kppu <- function(...) { paste(..., sep = '_',  collapse = '_') } # kollapse by underscore
-kpps <- function(...) { paste(..., sep = '/', collapse = '/') } # kollapse by (forward) slash
-kppd <- function(...) { paste(..., sep = '-', collapse = '-') } # kollapse by dash
-
-sppp <- function(...) { # Simplified Paste by point
-  string <- paste(..., sep = '.')
-  gsub(pattern = '\\.+', replacement = '\\.', x = string)
-}
-
 
 ## Generic -------------------------------------------------------------------------------------------------
 '%!in%' <- function(x,y)!('%in%'(x,y))
@@ -796,80 +781,6 @@ lookup <- function(needle, haystack, exact = TRUE, report = FALSE) { # Awesome p
 }
 
 
-
-## String operations  -------------------------------------------------------------------------------------------------
-parsepvalue <- function(pvalue = 0.01) paste0("(p<",pvalue,")"); # Parse p-value from a number to a string.
-
-eval_parse_kollapse <- function(...) { # evaluate and parse (dyn_var_caller)
-  substitute(eval(parse(text = kollapse( ... , print = FALSE))))
-}
-
-
-param.list.2.fname <- function(ls.of.params = p) { # Take a list of parameters and parse a string from their names and values.
-  paste(names(ls.of.params), ls.of.params, sep = ".", collapse = "_")
-}
-
-
-PasteDirNameFromFlags <- function(...) { # Paste a dot (point) separated string from a list of inputs (that can be empty), and clean up the output string from dot multiplets (e.g: ..).
-  flagList <- c(...)
-  pastedFlagList <- kpp(flagList)
-  CleanDirName <- gsub(x = pastedFlagList, pattern = '[\\..] + ',replacement = '\\.' )
-  return(CleanDirName)
-}
-# PasteDirNameFromFlags("HCAB"
-#                       , flag.nameiftrue(p$'premRNA')
-#                       , flag.nameiftrue(p$"dSample.Organoids")
-#                       , flag.names_list(p$'variables.2.regress')
-#                       ,  flag.nameiftrue(p$'Man.Int.Order') )
-
-
-### File name and path parsing ------------------------------------------------------------------------------------------------
-PasteOutdirFromFlags <- function(path = "~/Dropbox/Abel.IMBA/AnalysisD", ...) { # Paste OutDir from (1) a path and (2) a from a list of inputs (that can be empty), and clean up the output string from dot and forward slash multiplets (e.g: ..).
-  flagList <- c(path, ...)
-  pastedFlagList <- kpp(flagList)
-  CleanDirName <- gsub(x = pastedFlagList, pattern = '[\\..] + ',replacement = '\\.' )
-  # pastedOutDir <- kpps(path, CleanDirName, "/")
-  pastedOutDir <- p0(CleanDirName, "/")
-  CleanDirName <- gsub(x = pastedOutDir, pattern = '[//] + ',replacement = '/' )
-  return(CleanDirName)
-}
-# PasteOutdirFromFlags("~/Dropbox/Abel.IMBA/AnalysisD/HCAB"
-#                      , flag.nameiftrue(p$'premRNA')
-#                      , flag.nameiftrue(p$"dSample.Organoids")
-#                      , flag.names_list(p$'variables.2.regress')
-#                      ,  flag.nameiftrue(p$'Man.Int.Order') )
-
-flag.name_value <- function(toggle, Separator = "_") { # Returns the name and its value, if its not FALSE.
-  if (!isFALSE(toggle)) {
-    output = paste(substitute(toggle), toggle, sep = Separator)
-    if (length(output) > 1) output = output[length(output)]  # fix for when input is a list element like p$'myparam'
-    return(output)
-  }
-}
-# Xseed = p$'seed' = F; flag.name_value(Xseed); flag.name_value(p$'seed')
-
-flag.nameiftrue <- function(toggle, prefix = NULL, suffix = NULL, name.if.not = "") { # Returns the name and its value, if its TRUE.
-  output = if (toggle) { paste0(prefix, (substitute(toggle)), suffix)
-  } else {paste0(prefix, name.if.not, suffix)}
-  if (length(output) > 1) output = output[length(output)]  # fix for when input is a list element like p$'myparam'
-  return(output)
-} # returns the name if its value is true
-nameiftrue = flag.nameiftrue # backward compatible
-
-flag.names_list <- function(par = p$'umap.min_dist') { # Returns the name and value of each element in a list of parameters.
-  if (length(par)) paste(substitute(par), kppu(par) , sep = "_")[[3]]
-};  # param.list.flag(par = p$umap.n_neighbors)
-
-
-flag.names_list.all.new <- function(pl = p.hm) { # Returns the name and value of each element in a list of parameters.
-  # if (length(pl)) paste(kppu(names(pl)), kppu(pl) , sep = "_")
-  if (length(pl)) kppd(paste(names(pl), pl, sep = "_"))
-}
-
-
-param.list.flag <- function(par = p$'umap.min_dist') { # Returns the name and value of each element in a list of parameters.
-  paste(substitute(par), par, sep = "_")[[3]]
-};  # param.list.flag(par = p$umap.n_neighbors)
 
 
 ## Matrix operations -------------------------------------------------------------------------------------------------
@@ -1753,5 +1664,4 @@ GC_content <- function(string, len = nchar(string), pattern = c("G","C")) { # GC
 
 # Temporary  ------------------------------------------------------------
 
-# TMP ------------------------------------------------------------------------------------------------
 
