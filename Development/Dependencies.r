92744acb03bdf6bbd0e76254ceb1e443098bcd24
vertesy <abel.vertesy@imba.oeaw.ac.at>
2021-11-04 20:56:27 +0100

    dependencies better declared

--- Development/Dependencies.r
+++ Development/Dependencies.r
@@ -1,39 +1,53 @@
 $`character(0)`
- [1] "any.duplicated"    "as.logical.wNames" "bind_rows"         "iprint"            "is_null"          
- [6] "join_all"          "llprint"           "mixedsort"         "na.omit.strip"     "stopif"           
-[11] "tibble"            "whist"            
+ [1] "any.duplicated"    "as.logical.wNames" "bind_rows"        
+ [4] "iprint"            "join_all"          "llprint"          
+ [7] "mixedsort"         "na.omit.strip"     "stopif"           
+[10] "tibble"           
 
 $`package:base`
- [1] "abs"           "any"           "apply"         "array"         "as.character"  "as.data.frame"
- [7] "as.factor"     "as.list"       "as.logical"    "as.matrix"     "as.numeric"    "as.vector"    
-[13] "attributes"    "c"             "cbind"         "ceiling"       "col"           "colMeans"     
-[19] "colnames"      "colSums"       "data.matrix"   "dim"           "dimnames"      "do.call"      
-[25] "duplicated"    "exp"           "factor"        "floor"         "grep"          "intersect"    
-[31] "is.data.frame" "is.finite"     "is.list"       "is.na"         "is.null"       "is.numeric"   
-[37] "is.table"      "is.vector"     "lapply"        "length"        "list"          "log"          
-[43] "log10"         "log2"          "match"         "match.call"    "matrix"        "max"          
-[49] "mean"          "merge"         "min"           "names"         "ncol"          "NCOL"         
-[55] "nrow"          "NROW"          "numeric"       "order"         "paste"         "paste0"       
-[61] "print"         "range"         "rank"          "rbind"         "Reduce"        "rep"          
-[67] "return"        "rev"           "round"         "row"           "row.names"     "rownames"     
-[73] "rowSums"       "sample"        "sapply"        "scale"         "seq"           "setdiff"      
-[79] "signif"        "sort"          "split"         "sqrt"          "stopifnot"     "substitute"   
-[85] "sum"           "t"             "table"         "tabulate"      "tapply"        "trimws"       
-[91] "unique"        "unlist"        "which"        
+ [1] "abs"           "any"           "apply"         "array"        
+ [5] "as.character"  "as.data.frame" "as.factor"     "as.list"      
+ [9] "as.logical"    "as.matrix"     "as.numeric"    "as.vector"    
+[13] "attributes"    "c"             "cbind"         "ceiling"      
+[17] "col"           "colMeans"      "colnames"      "colSums"      
+[21] "data.matrix"   "dim"           "dimnames"      "do.call"      
+[25] "duplicated"    "exp"           "factor"        "floor"        
+[29] "grep"          "intersect"     "is.data.frame" "is.finite"    
+[33] "is.list"       "is.na"         "is.null"       "is.numeric"   
+[37] "is.table"      "is.vector"     "lapply"        "length"       
+[41] "list"          "log"           "log10"         "log2"         
+[45] "match"         "match.call"    "matrix"        "max"          
+[49] "mean"          "merge"         "min"           "names"        
+[53] "ncol"          "NCOL"          "nrow"          "NROW"         
+[57] "numeric"       "order"         "paste"         "paste0"       
+[61] "print"         "range"         "rank"          "rbind"        
+[65] "Reduce"        "rep"           "return"        "rev"          
+[69] "round"         "row"           "row.names"     "rownames"     
+[73] "rowSums"       "sample"        "sapply"        "scale"        
+[77] "seq"           "setdiff"       "signif"        "sort"         
+[81] "split"         "sqrt"          "stopifnot"     "substitute"   
+[85] "sum"           "t"             "table"         "tabulate"     
+[89] "tapply"        "trimws"        "unique"        "unlist"       
+[93] "which"        
 
 $`package:CodeAndRoll2`
- [1] "arr.of.lists.2.df"           "as.named.vector"             "colMedians"                 
- [4] "copy.dimension.and.dimnames" "grepv"                       "idim"                       
- [7] "intermingle2vec"             "matrix.fromNames"            "sem"                        
-[10] "symdiff"                     "unlapply"                    "vec.fromNames"              
+ [1] "arr.of.lists.2.df"           "as.named.vector"            
+ [3] "colMedians"                  "copy.dimension.and.dimnames"
+ [5] "grepv"                       "idim"                       
+ [7] "intermingle2vec"             "matrix.fromNames"           
+ [9] "sem"                         "symdiff"                    
+[11] "unlapply"                    "vec.fromNames"              
 [13] "which_names"                
+
+$`package:graphics`
+[1] "abline" "hist"  
 
 $`package:methods`
 [1] "is"
 
 $`package:stats`
- [1] "filter"     "median"     "na.exclude" "na.omit"    "quantile"   "rnorm"      "rpois"     
- [8] "sd"         "setNames"   "var"       
+ [1] "filter"     "median"     "na.exclude" "na.omit"    "quantile"   "rnorm"     
+ [7] "rpois"      "sd"         "setNames"   "var"       
 
 $`package:Stringendo`
 [1] "percentage_formatter"
@@ -47,6 +61,7 @@ $`package:utils`
 character(0)
 base
 CodeAndRoll2
+graphics
 methods
 stats
 Stringendo
