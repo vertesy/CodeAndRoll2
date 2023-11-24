eadbf2a4ff8e878b5589747c14d779bfa5e06ffd
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-24 15:06:13 +0100

    Create deprecated.R

---
+++ R/deprecated.R
@@ -0,0 +1,18 @@
+
+# _________________________________________________________________________________________________
+#' #' @title sstrsplit
+#' #'
+#' #' @description Alias for `str_split_fixed` in the `stringr` package.
+#' #' @param string A string.
+#' #' @param pattern A character string pattern to split on.
+#' #' @param n The number of elements to return in each split.
+#' #' @return A list of character vectors.
+#' #' @seealso
+#' #'  \code{\link[stringr]{str_split}}
+#' #' @importFrom stringr str_split_fixed
+#' #' @export
+#' sstrsplit <- function(string, pattern = "_", n = F) {
+#'   if (!n) n <- stringr::str_count(string = string, pattern = pattern)
+#'   stringr::str_split_fixed(string, pattern = pattern, n = n)
+#'   }
+# "replace with str_split_1"
