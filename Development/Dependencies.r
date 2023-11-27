a1ef438c93dcaf3e81b2953a97a55cdc4af9e32b
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-27 16:53:24 +0100

    ...

--- Development/Dependencies.r
+++ Development/Dependencies.r
@@ -1,50 +1,43 @@
+Dependency file generated on Mon Nov 27 16:52:49 2023 
+
+#################################################################################################### 
+CodeAndRoll2.R
+#################################################################################################### 
 $`character(0)`
-[1] "bind_rows" "join_all"  "l"         "mixedsort" "stopif"   
-[6] "tibble"   
+ [1] "as.named.vector"         "bind_rows"               "FirstCol2RowNames"       "FirstCol2RowNames.as.df"
+ [5] "full_join"               "join_all"                "map"                     "map_chr"                
+ [9] "mixedsort"               "qbarplot"                "str_pad"                 "tibble"                 
 
 $`package:base`
- [1] "abs"           "any"           "apply"        
- [4] "array"         "as.character"  "as.data.frame"
- [7] "as.factor"     "as.list"       "as.logical"   
-[10] "as.matrix"     "as.numeric"    "as.vector"    
-[13] "attributes"    "c"             "ceiling"      
-[16] "col"           "colMeans"      "colnames"     
-[19] "colSums"       "data.frame"    "data.matrix"  
-[22] "dim"           "dimnames"      "do.call"      
-[25] "duplicated"    "exp"           "factor"       
-[28] "floor"         "grep"          "intersect"    
-[31] "is.data.frame" "is.finite"     "is.list"      
-[34] "is.na"         "is.null"       "is.numeric"   
-[37] "is.table"      "is.vector"     "lapply"       
-[40] "length"        "list"          "log"          
-[43] "log10"         "log2"          "make.names"   
-[46] "match"         "match.call"    "matrix"       
-[49] "max"           "mean"          "merge"        
-[52] "min"           "names"         "ncol"         
-[55] "NCOL"          "nrow"          "NROW"         
-[58] "numeric"       "order"         "paste"        
-[61] "paste0"        "print"         "range"        
-[64] "rank"          "rbind"         "Reduce"       
-[67] "rep"           "return"        "rev"          
-[70] "round"         "row"           "row.names"    
-[73] "rownames"      "rowSums"       "sample"       
-[76] "sapply"        "scale"         "seq"          
-[79] "setdiff"       "signif"        "sort"         
-[82] "split"         "sqrt"          "stopifnot"    
-[85] "sum"           "t"             "table"        
-[88] "tabulate"      "tapply"        "trimws"       
-[91] "unique"        "unlist"        "which"        
+  [1] ".Deprecated"      "abs"              "all"              "any"              "apply"           
+  [6] "array"            "as.character"     "as.data.frame"    "as.factor"        "as.list"         
+ [11] "as.logical"       "as.matrix"        "as.numeric"       "as.vector"        "attributes"      
+ [16] "c"                "cat"              "ceiling"          "class"            "col"             
+ [21] "colMeans"         "colnames"         "colSums"          "data.frame"       "data.matrix"     
+ [26] "dim"              "dimnames"         "do.call"          "duplicated"       "exp"             
+ [31] "factor"           "floor"            "gl"               "grep"             "ifelse"          
+ [36] "intersect"        "is.character"     "is.data.frame"    "is.factor"        "is.finite"       
+ [41] "is.list"          "is.logical"       "is.na"            "is.null"          "is.numeric"      
+ [46] "is.table"         "is.vector"        "lapply"           "length"           "list"            
+ [51] "log"              "log10"            "make.names"       "match"            "match.call"      
+ [56] "matrix"           "max"              "mean"             "merge"            "min"             
+ [61] "names"            "ncol"             "NCOL"             "nrow"             "NROW"            
+ [66] "numeric"          "order"            "paste"            "paste0"           "print"           
+ [71] "range"            "rbind"            "Reduce"           "rep"              "return"          
+ [76] "rev"              "round"            "row"              "row.names"        "rownames"        
+ [81] "rowSums"          "sample"           "sapply"           "scale"            "seq"             
+ [86] "setdiff"          "signif"           "sort"             "split"            "sqrt"            
+ [91] "stop"             "stopifnot"        "substitute"       "sum"              "suppressWarnings"
+ [96] "t"                "table"            "tabulate"         "tapply"           "trimws"          
+[101] "unique"           "unlist"           "warning"          "which"           
 
 $`package:CodeAndRoll2`
- [1] "any.duplicated"              "arr.of.lists.2.df"          
- [3] "as.logical.wNames"           "as.named.vector"            
- [5] "colMedians"                  "copy.dimension.and.dimnames"
- [7] "grepv"                       "idim"                       
- [9] "intermingle2vec"             "matrix.fromNames"           
-[11] "na.omit.strip"               "sem"                        
-[13] "symdiff"                     "union.ls"                   
-[15] "unlapply"                    "vec.fromNames"              
-[17] "which_names"                
+ [1] "any.duplicated"              "arr.of.lists.2.df"           "as.logical.wNames"          
+ [4] "colMedians"                  "copy.dimension.and.dimnames" "get_col_types"              
+ [7] "grepv"                       "idim"                        "intermingle2vec"            
+[10] "matrix.fromNames"            "na.omit.strip"               "pc_TRUE"                    
+[13] "sem"                         "symdiff"                     "union.ls"                   
+[16] "unlapply"                    "vec.fromNames"               "which_names"                
 
 $`package:graphics`
 [1] "abline" "hist"  
@@ -53,18 +46,14 @@ $`package:methods`
 [1] "is"
 
 $`package:stats`
- [1] "filter"     "median"     "na.exclude" "na.omit"   
- [5] "quantile"   "rnorm"      "rpois"      "sd"        
+ [1] "filter"     "median"     "na.exclude" "na.omit"    "quantile"   "rnorm"      "rpois"      "sd"        
  [9] "setNames"   "var"       
 
 $`package:Stringendo`
 [1] "iprint"               "percentage_formatter"
 
-$`package:stringr`
-[1] "str_pad"         "str_split_fixed"
-
 $`package:utils`
-[1] "head" "str"  "tail"
+[1] "head"  "stack" "str"   "tail" 
 
 character(0)
 base
@@ -73,5 +62,9 @@ graphics
 methods
 stats
 Stringendo
-stringr
 utils
+#################################################################################################### 
+deprecated.R
+#################################################################################################### 
+logical(0)
+
