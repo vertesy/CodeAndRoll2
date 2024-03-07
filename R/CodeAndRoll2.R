b8b3a547f91b922b0874856c2597d9618f57e82a
vertesy <abel.vertesy@imba.oeaw.ac.at>
2024-03-07 13:08:19 +0100

    fun getScriptName, getProject

--- R/CodeAndRoll2.R
+++ R/CodeAndRoll2.R
@@ -11,7 +11,52 @@
 ### CHAPTERS:
 
 # _________________________________________________________________________________________________
-## Assertions ____________________________________________________________ ----
+## Rstudio based context retrieval ____________________________________________________________ ----
+
+#' @title Get Current Script Name or Basename of Output Directory
+#'
+#' @description This function attempts to retrieve the name of the currently opened script in
+#' the RStudio editor. If the script name cannot be obtained or if the `rstudioapi` package is
+#' not available, it returns the basename of the directory specified by `OutDir`.
+#'
+#' @return A string containing the basename of the current script or the basename of `OutDir`
+#' if the script name is unavailable.
+#' @importFrom rstudioapi getSourceEditorContext
+#'
+#' @export
+getScriptName <- function() {
+  # Check if rstudioapi is available
+  if (!requireNamespace("rstudioapi", quietly = TRUE)) {
+    message("rstudioapi package is not available. Please install it using install.packages('rstudioapi').")
+  } else {
+    scriptName <- basename(rstudioapi::getSourceEditorContext()$path)
+  }
+
+  # If scriptName is empty, return basename of OutDir
+  # Can happen at an unsaved file, etc.
+  if (scriptName == "") scriptName <- basename(OutDir)
+
+  return(scriptName)
+}
+
+# _________________________________________________________________________________________________
+#' @title getProject
+#'
+#' @description Try to get the project name you are wokring on in Rstudio.
+#' @returns The final subfolder of your project, or NULL, if you are not running one
+#' @importFrom rstudioapi getActiveProject
+#' @examples getProject()
+#'
+#' @export
+getProject <- function() {
+  if (!requireNamespace("rstudioapi", quietly = TRUE)) {
+    warning("rstudioapi package is not available. Please install it using install.packages('rstudioapi').",
+            immediate. = TRUE)
+  } else {
+    tryCatch(basename(rstudioapi::getActiveProject()), error = function(e) {})
+  }
+}
+
 
 
 # _________________________________________________________________________________________________
