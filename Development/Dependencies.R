f83a773b55b7bb4edddae59306b797663a6a8d15
vertesy <abel.vertesy@imba.oeaw.ac.at>
2024-10-25 22:22:18 +0200

    ...

--- Development/Dependencies.R
+++ Development/Dependencies.R
@@ -1,2 +1,70 @@
-Dependency file generated on Wed Oct 23 18:20:57 2024 
+Dependency file generated on Mon Nov 27 16:52:49 2023 
+
+#################################################################################################### 
+CodeAndRoll2.R
+#################################################################################################### 
+$`character(0)`
+ [1] "as.named.vector"         "bind_rows"               "FirstCol2RowNames"       "FirstCol2RowNames.as.df"
+ [5] "full_join"               "join_all"                "map"                     "map_chr"                
+ [9] "mixedsort"               "qbarplot"                "str_pad"                 "tibble"                 
+
+$`package:base`
+  [1] ".Deprecated"      "abs"              "all"              "any"              "apply"           
+  [6] "array"            "as.character"     "as.data.frame"    "as.factor"        "as.list"         
+ [11] "as.logical"       "as.matrix"        "as.numeric"       "as.vector"        "attributes"      
+ [16] "c"                "cat"              "ceiling"          "class"            "col"             
+ [21] "colMeans"         "colnames"         "colSums"          "data.frame"       "data.matrix"     
+ [26] "dim"              "dimnames"         "do.call"          "duplicated"       "exp"             
+ [31] "factor"           "floor"            "gl"               "grep"             "ifelse"          
+ [36] "intersect"        "is.character"     "is.data.frame"    "is.factor"        "is.finite"       
+ [41] "is.list"          "is.logical"       "is.na"            "is.null"          "is.numeric"      
+ [46] "is.table"         "is.vector"        "lapply"           "length"           "list"            
+ [51] "log"              "log10"            "make.names"       "match"            "match.call"      
+ [56] "matrix"           "max"              "mean"             "merge"            "min"             
+ [61] "names"            "ncol"             "NCOL"             "nrow"             "NROW"            
+ [66] "numeric"          "order"            "paste"            "paste0"           "print"           
+ [71] "range"            "rbind"            "Reduce"           "rep"              "return"          
+ [76] "rev"              "round"            "row"              "row.names"        "rownames"        
+ [81] "rowSums"          "sample"           "sapply"           "scale"            "seq"             
+ [86] "setdiff"          "signif"           "sort"             "split"            "sqrt"            
+ [91] "stop"             "stopifnot"        "substitute"       "sum"              "suppressWarnings"
+ [96] "t"                "table"            "tabulate"         "tapply"           "trimws"          
+[101] "unique"           "unlist"           "warning"          "which"           
+
+$`package:CodeAndRoll2`
+ [1] "any.duplicated"              "arr.of.lists.2.df"           "as.logical.wNames"          
+ [4] "colMedians"                  "copy.dimension.and.dimnames" "get_col_types"              
+ [7] "grepv"                       "idim"                        "intermingle2vec"            
+[10] "matrix.fromNames"            "na.omit.strip"               "pc_TRUE"                    
+[13] "sem"                         "symdiff"                     "union.ls"                   
+[16] "unlapply"                    "vec.fromNames"               "which_names"                
+
+$`package:graphics`
+[1] "abline" "hist"  
+
+$`package:methods`
+[1] "is"
+
+$`package:stats`
+ [1] "filter"     "median"     "na.exclude" "na.omit"    "quantile"   "rnorm"      "rpois"      "sd"        
+ [9] "setNames"   "var"       
+
+$`package:Stringendo`
+[1] "iprint"               "percentage_formatter"
+
+$`package:utils`
+[1] "head"  "stack" "str"   "tail" 
+
+character(0)
+base
+CodeAndRoll2
+graphics
+methods
+stats
+Stringendo
+utils
+#################################################################################################### 
+deprecated.R
+#################################################################################################### 
+logical(0)
 
